338dfbbb869377342cc469b71f7c2b3a4398ca31
eliszewski <liszewski31098@gmail.com>
2022-12-11 12:41:23 -0500

    done

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,8 +3,13 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    Character cs;
+    Character cf;
+
 
     ROT13(Character cs, Character cf) {
+        this.cf = cf;
+        this.cs = cs;
     }
 
     ROT13() {
@@ -12,21 +17,27 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        String s = "";
+        for (Character c : text.toCharArray()
+             ) {
+            String temp = c.toString();
+            if(temp.matches("[a-z]")) s += (char)(((c - (97 - (this.cf - this.cs))) % 26) + 97);
+            if (temp.matches("[A-Z]")) s += (char) (((c - (65 - (this.cf - this.cs))) % 26) + 65);
+            if (!temp.matches("[a-zA-Z]")) s += temp;
+        }
+        return s;
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
-
-        return "";
+        return s.substring(s.indexOf(c)) + s.substring(0,s.indexOf(c));
     }
 
 }
